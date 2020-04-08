d36923c42bbf4d3870e5df5616fc76b404cf2cd3
lizhichao <948341865@qq.com>
2020-04-08 18:06:29 +0800

    user、taoke模块搭建

---
+++ src/main/java/com/dawn/taobaoke/TaobaokeApplication.java
@@ -0,0 +1,13 @@
+package com.dawn.taobaoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaobaokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaobaokeApplication.class, args);
+	}
+
+}
